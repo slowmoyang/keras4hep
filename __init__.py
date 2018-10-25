acf27de838bdcb82c56d81e7fa8cde2fcebaee44
slowmoyang <slowmoyang@gmail.com>
2018-10-25 15:54:28 +0900

    Add __init__ files

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from keras4hep import data
+from keras4hep import callbacks
+from keras4hep import projects
---
+++ projects/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras4hep.projects import qgjets
+from keras4hep.projects import deepshowersim
+from keras4hep.projects import bayesian
+from keras4hep.projects import vts
