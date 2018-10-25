541543fbd82ecc833fee1e947e4dab1e079cb4c4
slowmoyang <slowmoyang@gmail.com>
2018-10-25 13:53:41 +0900

    Update keras4hep.data.__init__

--- data/__init__.py
+++ data/__init__.py
@@ -4,7 +4,10 @@ from __future__ import print_function
 
 
 from keras4hep.data import dataset
+from keras4hep.data import batch
 from keras4hep.data import data_iter
+from keras4hep.data import utils
 
 from keras4hep.data.dataset import BaseTreeDataset 
 from keras4hep.data.data_iter import DataIterator
+from keras4hep.data.utils import get_batch_time
