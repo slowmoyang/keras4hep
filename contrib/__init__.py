26d2be508c25974a6f05da4a696cdab0e2ba7d71
slowmoyang <slowmoyang@gmail.com>
2018-10-25 13:47:30 +0900

    Add a contributed libraries (my wish)

---
+++ contrib/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras4hep.contrib import qgjets
+from keras4hep.contrib import deepshowersim
+from keras4hep.contrib import bayesian
+from keras4hep.contrib import vts
---
+++ contrib/bayesian/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras4hep.contrib.bayesian import dropout_inference
+from keras4hep.contrib.bayesian import models
