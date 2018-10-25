61449432260fb6a13a24d2047c02ba8d99398736
slowmoyang <slowmoyang@gmail.com>
2018-10-25 15:07:55 +0900

    Change the name of 'check_batch_time' to 'get_batch_time'. and add a verbosity arg to

--- data/utils.py
+++ data/utils.py
@@ -6,16 +6,17 @@ import time
 
 import numpy as np
 
-def check_batch_time(data_iter, num_iter=10):
+def get_batch_time(data_iter, num_iter=10, verbose=True):
     elapsed_times = []
     for _ in range(num_iter):
         start_time = time.time()
         data_iter.next()
         elapsed_times.append(time.time() - start_time)
     elapsed_times = np.array(elapsed_times)
-    print("[Elapsed time] mean: {mean:.5f} / stddev {stddev:5f}".format(
-        mean=elapsed_times.mean(),
-        stddev=elapsed_times.std()))
-    print("Batch Size: {}".format(data_iter.batch_size))
-    print("Iteration: {}".format(num_iter))
+    if verbose:
+        print("[Elapsed time] mean: {mean:.5f} / stddev {stddev:5f}".format(
+            mean=elapsed_times.mean(),
+            stddev=elapsed_times.std()))
+        print("Batch Size: {}".format(data_iter.batch_size))
+        print("Iteration: {}".format(num_iter))
     return elapsed_times
