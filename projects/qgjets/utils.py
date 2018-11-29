7d66832b979ab495f9536f59aa226506b5750104
slowmoyang <slowmoyang@gmail.com>
2018-11-29 17:07:58 +0900

    recover

--- projects/qgjets/utils.py
+++ projects/qgjets/utils.py
@@ -38,28 +38,29 @@ def get_dataset_paths(min_pt):
 
 
 def find_best_checkpoint(log_dir):
-    
     if log_dir.endswith("/"):
         name = os.path.basename(log_dir.rstrip("/"))
     else:
         name = os.path.basename(log_dir)
-    
-    roc_dir = os.path.join(log_dir, "roc_curve")
-    roc_curve = os.listdir(roc_dir)
+
+    roc_curve_dir = os.path.join(log_dir, "roc_curve")
+    roc_curve = [each for each in os.listdir(roc_curve_dir)]
+
     if len(roc_curve) == 0:
         return None
+
     roc_curve = [os.path.splitext(each)[0] for each in roc_curve]
     roc_curve = [parse_str(each) for each in roc_curve]
     best = max(roc_curve, key=lambda each: each["auc"])
+
     best["name"] = name
     return best
 
 
 def get_ranking(logs, write=True):
-    
     log_dirs = [os.path.join(logs, each) for each in os.listdir(logs)]
     log_dirs = [each for each in log_dirs if os.path.isdir(each)]
-    
+
     data = []
     for each in log_dirs:
         datum = find_best_checkpoint(each)
