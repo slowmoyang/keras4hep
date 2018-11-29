041560b266249ca5ce87247b01e03366d73aae4b
slowmoyang <slowmoyang@gmail.com>
2018-11-29 16:57:29 +0900

    Add a function to get best checkpoint for the given project directory

--- projects/qgjets/utils.py
+++ projects/qgjets/utils.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import os
+import pandas as pd
+
+from keras4hep.utils.misc import parse_str
 
 def get_dataset_paths(min_pt):
     max_pt = int(min_pt * 1.1)
@@ -33,3 +35,42 @@ def get_dataset_paths(min_pt):
 
     paths["preprocessing"] = prep_path
     return paths
+
+
+def find_best_checkpoint(log_dir):
+    
+    if log_dir.endswith("/"):
+        name = os.path.basename(log_dir.rstrip("/"))
+    else:
+        name = os.path.basename(log_dir)
+    
+    roc_dir = os.path.join(log_dir, "roc_curve")
+    roc_curve = os.listdir(roc_dir)
+    if len(roc_curve) == 0:
+        return None
+    roc_curve = [os.path.splitext(each)[0] for each in roc_curve]
+    roc_curve = [parse_str(each) for each in roc_curve]
+    best = max(roc_curve, key=lambda each: each["auc"])
+    best["name"] = name
+    return best
+
+
+def get_ranking(logs, write=True):
+    
+    log_dirs = [os.path.join(logs, each) for each in os.listdir(logs)]
+    log_dirs = [each for each in log_dirs if os.path.isdir(each)]
+    
+    data = []
+    for each in log_dirs:
+        datum = find_best_checkpoint(each)
+        if datum is None:
+            continue
+        data.append(datum)
+    df = pd.DataFrame(data)
+    df = df.sort_values(by="auc", ascending=False)
+
+    if write:
+        out_path = os.path.join(logs, "ranking.csv")
+        df.to_csv(out_path)
+
+    return df
