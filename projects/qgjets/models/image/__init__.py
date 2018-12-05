d744705bf6ab0084f1da2d9064f9bf41f7999355
slowmoyang <slowmoyang@gmail.com>
2018-12-05 23:44:55 +0900

    Remove codes about SOTA ConvNets. Use keras_applications

--- projects/qgjets/models/image/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from keras4hep.projects.qgjets.models.image import layer_utils
-from keras4hep.projects.qgjets.models.image.layer_utils import *
-from keras4hep.projects.qgjets.models.image import densenet
